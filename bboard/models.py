a0060726c1db7d9c1ab4e38e6c9d0af976355aee
Vladosik <smolnikvladislav@gmail.com>
2023-01-12 01:08:39 +0300

    info about django added

--- bboard/models.py
+++ bboard/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here. то есть для моделей (классов-таблиц)
 
 class product (models.Model):
@@ -17,3 +18,35 @@ class FirstModule(models.Model):
 
     def __str__(self):                      #Можно сделать вывод инфы о строке
         return f"{self.id} -- {self.field_1} -- {self.field_5}"
+
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='book name', # переименование поля в поле админа
+        help_text='Book name', #текст, который описывает поле снизу поля в админке
+        #default="Unknown book" #параметр по умолчанию
+        #unique=True #уникальность
+        #unique_for_date = 'published_date' #то есть работает как валидация, что нельзя вводить одно и то же поля в заданный период
+        #null=True #разрешает не заполнять поле, конфликтует с default
+        blank=True, #разрешает хранить пустые значения (не null)
+        db_index=True, #сокращает поиск, ПРОЧИТАТЬ
+        db_column='change_name' #просто переименовать поле
+
+    )
+
+    published_date = models.DateTimeField(
+        null=True,
+        editable=False #запрещаем изменять поле через админ панель
+
+    )
+
+    key=models.IntegerField(
+        primary_key=True
+    )
+
+    #slug = models.slugField(null=True, editable=False)
+
+   # def save(self, *a, )
+
